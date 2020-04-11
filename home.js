eb585e33045d183ccb791dded98b3c0445c3e1d6
Calvin-Coding <60584878+Calvin-Coding@users.noreply.github.com>
2020-04-11 10:06:19 -0700

    Added Menu
    Click on the Icon and the menu opens.
    
    - Calvin-Coding

---
+++ home.js
@@ -0,0 +1,7 @@
+var menu_close = function() {
+    document.getElementById("menu").style.display = "none";
+}
+
+var menu_open = function() {
+    document.getElementById("menu").style.display = "block";
+}
